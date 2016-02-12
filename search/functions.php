4fced9f5db9f9c995f9acd2ed5e3897eab4ac953
adrq <adrq22@yahoo.com>
2016-02-12 13:32:27 -0500

    update solr schema + start of PHP backend

---
+++ search/functions.php
@@ -0,0 +1,18 @@
+<?php 
+
+/**
+ * @file functions.php
+ * Functions used throughout the website.
+ */
+ 
+ 
+
+/*
+ * function getResultsFromSolr
+ * performs search on solr and returns ids of mathing documents
+ * @param {array} $query: associative array of search parameters eg: "title" => "query title"
+ * @return {array} or {string} if an error ocurred
+ */
+ function getResultsFromSolr($query){
+   
+ }
