d7c7528da8307fd0fcd3f180d2c69ed24d8e5ff8
adrq <adrq22@yahoo.com>
2016-02-15 12:36:58 -0500

    updated functions.php

---
+++ search/config.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+config file for global values
+*/
+
+global $solrCoreName;
+$solrCoreName = "duss-indexing";
--- search/functions.php
+++ search/functions.php
@@ -1,18 +1,62 @@
-<?php 
+<?php
 
 /**
  * @file functions.php
- * Functions used throughout the website.
- */
- 
- 
+* Functions used throughout the website.
+*/
+
+
+require_once('config.php');
+
+
 
 /*
  * function getResultsFromSolr
- * performs search on solr and returns ids of mathing documents
+* performs search on solr and returns ids of mathing documents
+* @param {array} $query: associative array of search parameters eg: "title" => "query title"
+* @return {array} or {FALSE} if an error ocurred
+*/
+function getResultsFromSolr($query){
+
+$queryString = buildSolrQuery($query);
+
+$jsonResponse = file_get_contents($queryString);
+
+if ($jsonResponse === false) return false;
+
+$responseArray = json_decode($jsonResponse,true);
+
+$searchResults = $responseArray["response"];
+
+print_r ($responseArray);
+
+print_r ($searchResults);
+
+return $searchResults;
+	 
+}
+
+
+/*
+ * function buildSolrQuery
+ * builds json query for solr based on parameters
  * @param {array} $query: associative array of search parameters eg: "title" => "query title"
- * @return {array} or {string} if an error ocurred
+ * @return {string} url-formatted solr query for json-formatted results
+
  */
- function getResultsFromSolr($query){
-   
- }
+function buildSolrQuery($query){
+
+$queryString = '';
+
+foreach ($query as $key => $value){
+  $queryString = $queryString.'&'.$key.'='.urlencode($value);
+}
+global $solrCoreName;
+
+$queryString = 'http://localhost:8983/solr/'.$solrCoreName.'/select?'.$queryString.'&wt=json&indent=true';
+
+return $queryString;
+}
+
+
+?>
