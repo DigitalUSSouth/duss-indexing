8172011ffe988abd9f64847bdf0f87f5ac442d67
adrq <adrq22@yahoo.com>
2018-03-06 16:26:43 -0500

    added dockerfile and requirements

--- web/duss.py
+++ web/duss.py
@@ -24,3 +24,6 @@ def feat_projects():
     with open('projects.json') as data_file:    
         projects = json.load(data_file)
     return render_template('featured-projects.html',title='Featured projects', projects=projects)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True,port=5001)
